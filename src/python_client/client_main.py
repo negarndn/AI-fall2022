f01453d73384885f2b9d736ed5bb7d8e7e56487a
parisa <prisashahbi@gmail.com>
2022-11-05 01:38:16 +0330

    add heuristic function and gem collecting methods to agent class

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -1,14 +1,110 @@
-import random
 from base import BaseAgent, Action
+from pathfinding import FindPath, Node
+from gem import Gem, GEMS_SCORES
+from operator import attrgetter
+from math import sqrt
 
+GEM_SEQUENCE_SCORE = [
+    [50,   0,   0, 0],
+    [50, 200, 100, 0],
+    [100, 50, 200, 100],
+    [50, 100, 50,  200],
+    [250, 50, 100, 50]
+]
+
+# TODO: Convert grid cells into nodes like path finding module
 class Agent(BaseAgent):
+    def __init__(self):
+        super(Agent, self).__init__()
+        self.actions = []
+        self.last_gem = 0
+        self.gems_list = []
+        self.agent = Node(0, 0)
+        self.find_gems()
+
+    def convert_path_to_action(self, final_path):
+        for i in range(len(final_path) - 1):
+            y = final_path[i][1] - final_path[i+1][1]
+            x = final_path[i][0] - final_path[i+1][0]
+            if x == 0:
+                if y == 1:
+                    self.actions.append(Action.RIGHT)
+                elif y == -1:
+                    self.actions.append(Action.LEFT)
+            if x == 1:
+                if y == 0:
+                    self.actions.append(Action.DOWN)
+                elif y == 1:
+                    self.actions.append(Action.DOWN_RIGHT)
+                elif y == -1:
+                    self.actions.append(Action.DOWN_LEFT)
+            if x == -1:
+                if y == 0:
+                    self.actions.append(Action.UP)
+                elif y == 1:
+                    self.actions.append(Action.UP_RIGHT)
+                elif y == -1:
+                    self.actions.append(Action.UP_LEFT)
+
+    def evaluate_gems(self, remaining_gems) -> list:
+        evaluated_gems = []
+        for gem in remaining_gems:
+            euclidean_distance = sqrt((self.agent.x - gem.x) ** 2 + (self.agent.y - gem.y) ** 2)
+            gem_seq_score = GEM_SEQUENCE_SCORE[self.last_gem][gem.type]
+            gem.evaluation_result = gem.score + gem_seq_score - euclidean_distance
+            evaluated_gems.append(gem)
+
+        return evaluated_gems
+
+    def choose_goal(self, remaining_gems) -> Gem:
+        evaluated_gems = self.evaluate_gems(remaining_gems)
+        for _ in range(len(evaluated_gems)):
+            most_valuable_gem = max(evaluated_gems, key=attrgetter('evaluation_result'))
+            manhattan_distance = abs(self.agent.x - most_valuable_gem.x) + abs(self.agent.y - most_valuable_gem.y)
+            if self.max_turn_count - self.turn_count < manhattan_distance:
+                evaluated_gems.remove(most_valuable_gem)
+                continue
+            else:
+                return most_valuable_gem
+        # TODO: Return Agent location as node if it does not find any valid goal
+
+    def is_gem(self, tile) -> bool:
+        return tile == "1" or tile == "2" or tile == "3" or tile == "4"
+
+    def find_gems(self):
+        for x in range(self.grid_height):
+            for y in range(self.grid_width):
+                if self.is_gem(self.grid[x][y]):
+                    gem = Gem()
+                    gem.x = x
+                    gem.y = y
+                    gem.type = int(self.grid[x][y])
+                    gem.score = GEMS_SCORES[self.grid[x][y]]
+                    self.gems_list.append(gem)
+
+    def generate_actions(self):
+        path = FindPath(self.grid, self.grid_height, self.grid_width)
+        goal = self.choose_goal(self.gems_list)
+        self.gems_list.remove(goal)
+        goal_location = (goal.x, goal.y)
+        agent_location = (self.agent.x, self.agent.y)
+        final_path = path.find_path(agent_location, goal_location)
+        self.convert_path_to_action(final_path)
 
     def do_turn(self) -> Action:
-        # map = Map(self.grid)
-        # print(map.get_show())
-        return random.choice(
-            [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.UP_LEFT,
-             Action.UP_RIGHT, Action.NOOP])
+        if len(self.actions) == 0:
+            self.generate_actions()
+        if len(self.actions) > self.max_turn_count:
+            self.actions = []
+            self.generate_actions()
+            # for _ in self.actions:
+            #     self.actions.append(Action.NOOP)
+        else:
+            return self.actions.pop(0)
+
+        # return random.choice(
+        #     [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.UP_LEFT,
+        #      Action.UP_RIGHT, Action.NOOP])
 
 
 if __name__ == '__main__':
--- src/python_client/pathfinding.py
+++ src/python_client/pathfinding.py
@@ -1,8 +1,145 @@
-from base import BaseAgent
-from src.server.logics.map import Map, Tile
+from math import sqrt
 
-def gem_sequence():
-    pass
 
-def find_path(grid, start, end):
-    pass
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.type = ''
+
+        # neighbors = []
+        self.previous = None
+        # self.obstacle = False
+
+
+class FindPath:
+    def __init__(self, initial_grid, height, width):
+        self.start = None
+        self.end = None
+        self.height = height
+        self.width = width
+        self.grid = self.create_grid(initial_grid)
+        self.open_set = []
+        self.closed_set = []
+        self.current_node = None
+        self.final_path = []
+
+    def find_path(self, start, end):
+        self.start = self.grid[start[0]][start[1]]
+        self.end = self.grid[end[0]][end[1]]
+        self.open_set.append(self.start)
+        while len(self.open_set) > 0:
+            self.a_star()
+            if len(self.final_path) > 0:
+                break
+
+        return self.final_path
+
+    def a_star(self):
+        best_way = 0
+        for i in range(len(self.open_set)):
+            if self.open_set[i].f < self.open_set[best_way].f:
+                best_way = i
+
+        self.current_node = self.open_set[best_way]
+        self.open_set.pop(best_way)
+        self.final_path = []
+        if self.current_node == self.end:
+            temp = self.current_node
+            while temp.previous:
+                self.final_path.append((temp.x, temp.y))
+                temp = temp.previous
+            self.final_path.append((self.start.x, self.start.y))
+
+        # open_set = self.clean_open_set(self.open_set, self.current_node)
+        # self.closed_set.append(self.current_node)
+
+        neighbors = self.find_neighbors(self.current_node)
+        for i, neighbor in enumerate(neighbors):
+            if self.can_go(neighbor):
+                # for diagonals neighbors
+                if self.is_diagonals(neighbor):
+                    temp_g = self.current_node.g + 2
+                else:
+                    temp_g = self.current_node.g + 1
+                control_flag = 0
+                if neighbor in self.open_set:
+                    if temp_g < neighbor.g:
+                        self.update_node(neighbor, temp_g)
+                else:
+                    self.update_node(neighbor, temp_g)
+                    self.open_set.append(neighbor)
+
+        self.closed_set.append(self.current_node)
+
+    def update_node(self, node, temp_g):
+        node.g = temp_g
+        node.h = self.h_score(node)
+        node.f = node.g + node.h
+        node.previous = self.current_node
+
+    def h_score(self, node):
+        distance = sqrt(abs(node.x - self.end.x)**2 + abs(node.y - self.end.y)**2)
+        return distance
+
+    def can_go(self, node):
+        if (node in self.closed_set) or (node.type == 'W'):
+            return False
+        # elif node.type == 'DOOR':
+        #     pass
+        else:
+            return True
+
+    def find_neighbors(self, node):
+        x = node.x
+        y = node.y
+        neighbors = []
+        if x < self.height - 1:
+            neighbors.append(self.grid[x + 1][y])
+        if x > 0:
+            neighbors.append(self.grid[x - 1][y])
+        if y < self.width - 1:
+            neighbors.append(self.grid[x][y + 1])
+        if y > 0:
+            neighbors.append(self.grid[x][y - 1])
+        # diagonals
+        if x > 0 and y > 0:
+            neighbors.append(self.grid[x - 1][y - 1])
+        if x < self.height - 1 and y > 0:
+            neighbors.append(self.grid[x + 1][y - 1])
+        if x > 0 and y < self.width - 1:
+            neighbors.append(self.grid[x - 1][y + 1])
+        if x < self.height - 1 and y < self.width - 1:
+            neighbors.append(self.grid[x + 1][y + 1])
+        return neighbors
+
+    def create_grid(self, initial_grid):
+        grid = []
+        for i in range(self.height):
+            grid.append([])
+            for j in range(self.width):
+                grid[-1].append(0)
+                grid[i][j] = Node(i, j)
+                grid[i][j].type = initial_grid[i][j]
+
+        return grid
+
+    def return_node(self, x, y):
+        node = self.grid[x][y]
+        print(f"type: {node.type}"
+              f"f: {node.f}")
+
+    def show_grid(self):
+        for i in range(self.height):
+            print()
+            for j in range(self.width):
+                print(f"{self.grid[i][j].type}, ", end='')
+        print()
+
+    def is_diagonals(self, node):
+        if abs(node.x - self.current_node.x) == 1 and abs(node.y - self.current_node.y) == 1:
+            return True
+
