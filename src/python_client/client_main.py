3a06d2a60e997b80b0dcece8f4bf0e63644d9231
Nafis Momeni <77436663+nafis-momeni@users.noreply.github.com>
2022-11-04 16:36:32 +0330

    add a* pathfinding module

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -1,11 +1,16 @@
 import random
 from base import BaseAgent, Action
+from pathfinding import FindPath
 
 class Agent(BaseAgent):
 
     def do_turn(self) -> Action:
-        # map = Map(self.grid)
-        # print(map.get_show())
+        # f = FindPath(self.grid, self.grid_height, self.grid_width)
+        # f.show_grid()
+        # f.find_path((0, 0), (2, 5))
+
+
+
         return random.choice(
             [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.UP_LEFT,
              Action.UP_RIGHT, Action.NOOP])
