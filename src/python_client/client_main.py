b73147675626afa98144fd0b6b4851db9f064e1e
parisa <prisashahbi@gmail.com>
2022-11-06 17:30:23 +0330

    fixed the biggest bug in tha worlddd

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -23,6 +23,7 @@ class Agent(BaseAgent):
         self.last_goal = Node(0, 0)
         self.gems_list = []
         self.finished = False
+        self.walls_count = 0
 
     def convert_path_to_action(self, final_path):
         for i in range(len(final_path) - 1):
@@ -54,12 +55,12 @@ class Agent(BaseAgent):
         for gem in remaining_gems:
             euclidean_distance = sqrt((self.agent.x - gem.x) ** 2 + (self.agent.y - gem.y) ** 2)
             gem_seq_score = GEM_SEQUENCE_SCORE[self.last_gem][int(gem.type)-1]
-            gem.evaluation_result = gem.score + gem_seq_score - euclidean_distance
+            gem.evaluation_result = gem_seq_score - (self.walls_count * euclidean_distance)
+            # gem.evaluation_result = gem.score + gem_seq_score - euclidean_distance
             evaluated_gems.append(gem)
         return evaluated_gems
 
     def choose_goal(self) -> list:
-        # remaining_gems = self.find_gems()
         evaluated_gems = self.evaluate_gems(self.gems_list)
         evaluated_gems.sort(key=attrgetter("evaluation_result"), reverse=True)
         for gem in evaluated_gems:
@@ -69,7 +70,7 @@ class Agent(BaseAgent):
                 continue
         if not len(evaluated_gems) == 0:
             return evaluated_gems
-        return self.agent
+        return []
 
     def find_gems(self) -> list:
         gems_list = []
@@ -78,45 +79,62 @@ class Agent(BaseAgent):
                 if self.grid[x][y] in ['1', '2', '3', '4']:
                     gem = Gem(x, y)
                     gem.type = self.grid[x][y]
-                    gem.score = GEM_SCORES[self.grid[x][y]]
+                    # gem.score = GEM_SCORES[self.grid[x][y]]
                     gems_list.append(gem)
         return gems_list
 
+    def wall_count(self):
+        for x in range(self.grid_height):
+            for y in range(self.grid_width):
+                if self.grid[x][y] == "W":
+                    self.walls_count += 1
+
     def generate_actions(self):
         self.agent = self.last_goal
+        print(f"las goal -> {self.last_goal.x, self.last_goal.y}")
         if not self.last_goal.type == '':
             self.last_gem = int(self.last_goal.type)
+            print(f"last gem: {self.last_gem}")
         f = FindPath(self.grid, self.grid_height, self.grid_width)
         goals_list = self.choose_goal()
+        for goal in goals_list:
+            print(f"x: {goal.x} y: {goal.y} type: {goal.type}")
         agent_location = (self.agent.x, self.agent.y)
         for goal in goals_list:
             goal_location = (goal.x, goal.y)
-            print(goal.x, goal.y)
+            print("------------------------------------------")
+            print(f"goal location: {goal_location}")
+            print(f"evaluation result -> {goal.evaluation_result}")
             final_path = f.find_path(agent_location, goal_location)
-            print("final")
-            print(final_path)
-            print("agent location")
-            print(agent_location)
-            if len(final_path) - 1 < self.max_turn_count - self.turn_count:
-                print("-------------------")
-                print(self.max_turn_count - self.turn_count, len(final_path) - 1)
+            print(f"final -> {final_path}")
+            print(f"agent location: {agent_location}")
+            if len(final_path) - 1 < self.max_turn_count - self.turn_count and not len(final_path) == 0:
+                print(f"self.max_turn_count - self.turn_count, len(final_path) - 1 -> {self.max_turn_count - self.turn_count, len(final_path) - 1}")
                 self.convert_path_to_action(final_path)
-                print(self.actions)
                 self.last_goal = goal
+                print("------------------------------------------")
                 return
-            else:
-                self.gems_list.pop(self.gems_list.index(goal))
-
         self.finished = True
 
     def do_turn(self) -> Action:
         self.gems_list = self.find_gems()
+        self.wall_count()
+        print(f"turn count: {self.turn_count}")
         if not self.finished:
+            print(f"self.finished is: {self.finished}")
             if len(self.actions) == 0:
+                print(f"generate actions call, turn: {self.turn_count}")
                 self.generate_actions()
-            return self.actions.pop(0)
+            if not self.finished:
+                print(f"action pop: {self.actions[0]}")
+                return self.actions.pop(0)
+            else:
+                print("no op called in else aval")
+                return Action.NOOP
         else:
+            print("no op called in else dovom")
             return Action.NOOP
+
 
 if __name__ == '__main__':
     data = Agent().play()
